9fc4890d56c7c0cd52eaf6994f8e4da746f571a8
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2013-04-06 21:30:34 +0100

    updating license header

--- Hubby/Interface/Repositories/MRCreateRepositoryWindowController.h
+++ Hubby/Interface/Repositories/MRCreateRepositoryWindowController.h
@@ -1,9 +1,23 @@
 //
-//  MRGistWindowController.h
+//  MRCreateRepositoryWindowController.h
 //  Hubby
 //
-//  Created by Marc Ransome on 25/03/2013.
-//  Copyright (c) 2013 fidgetbox. All rights reserved.
+//  Copyright (c) 2013, Marc Ransome <marc.ransome@fidgetbox.co.uk>
+//
+//  This file is part of Hubby.
+//
+//  Hubby is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Hubby is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Hubby.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <Cocoa/Cocoa.h>
